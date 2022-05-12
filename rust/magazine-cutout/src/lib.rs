9f9508cfe09c86f096bf6bda0c07d136bb1ed38b
yoshimaru46 <yshr446@gmail.com>
2022-05-12 13:04:40 +0900

    wip

---
+++ rust/magazine-cutout/src/lib.rs
@@ -0,0 +1,26 @@
+// This stub file contains items that aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+use std::collections::HashMap;
+
+pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
+    let mut available_words: HashMap<&str, usize> = HashMap::new();
+    let mut needed_words: HashMap<&str, usize> = HashMap::new();
+
+    for word in magazine {
+        let count = available_words.entry(word).or_insert(0);
+        *count += 1;
+    }
+    for word in note {
+        let count = needed_words.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    for (word, count) in needed_words {
+        if *available_words.entry(word).or_default() < count {
+            return false;
+        }
+    }
+    true
+}
