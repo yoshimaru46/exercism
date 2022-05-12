d0b2b47a2806d5d8d2800b2b6fd9e7ba69d76873
yoshimaru46 <yshr446@gmail.com>
2022-05-13 03:36:49 +0900

    Proverb

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    if list.is_empty() {
+        return String::new();
+    }
+    list.windows(2)
+        .map(|w| format!("For want of a {} the {} was lost.", w[0], w[1]))
+        .chain(std::iter::once(format!(
+            "And all for the want of a {}.",
+            list[0]
+        )))
+        .collect::<Vec<_>>()
+        .join("\n")
 }
