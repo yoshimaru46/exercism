35d657ea72c1381bfb15d840c1a4dc713f328258
yoshimaru46 <yshr446@gmail.com>
2022-05-13 03:33:22 +0900

    wip

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,3 +1,14 @@
+fn is_yelling(message: &str) -> bool {
+    let have_letters: bool = message.chars().filter(|x| x.is_alphabetic()).count() > 0;
+    message.to_uppercase() == message && have_letters
+}
+
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+    match message.trim() {
+        m if m.trim().len() == 0 => "Fine. Be that way!",
+        m if m.ends_with("?") && is_yelling(m) => "Calm down, I know what I'm doing!",
+        m if m.ends_with("?") => "Sure.",
+        m if is_yelling(m) => "Whoa, chill out!",
+        _ => "Whatever.",
+    }
 }
