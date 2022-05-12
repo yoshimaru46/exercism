b47605bc98b3e32dafb894acfb38bfa0e0fe3f32
yoshimaru46 <yshr446@gmail.com>
2022-05-12 16:09:22 +0900

    wip

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,7 +1,10 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    if !(1..=64).contains(&s) {
+        panic!("Square must be between 1 and 64");
+    }
+    2_u64.pow(s - 1)
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    (1..=64).into_iter().map(square).sum()
 }
