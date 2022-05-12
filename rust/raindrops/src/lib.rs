cc00f0c7f294481a541aa1d48480676096e59591
yoshimaru46 <yshr446@gmail.com>
2022-05-12 16:25:38 +0900

    wip

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,23 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    fn is_factor(a: u32, b: u32) -> bool {
+        b % a == 0
+    }
+
+    let mut ans = String::new();
+
+    if is_factor(3, n) {
+        ans.push_str("Pling");
+    }
+    if is_factor(5, n) {
+        ans.push_str("Plang");
+    }
+    if is_factor(7, n) {
+        ans.push_str("Plong");
+    }
+
+    if ans.is_empty() {
+        ans.push_str(&n.to_string());
+    }
+
+    ans
 }
