275d90a22d21149f8c7272a89076112bfa19a68f
yoshimaru46 <yshr446@gmail.com>
2022-05-12 12:46:07 +0900

    wip

--- rust/role-playing-game/src/lib.rs
+++ rust/role-playing-game/src/lib.rs
@@ -2,6 +2,8 @@
 // to enable stricter warnings.
 #![allow(unused)]
 
+use std::cmp::min;
+
 pub struct Player {
     pub health: u32,
     pub mana: Option<u32>,
@@ -10,10 +12,31 @@ pub struct Player {
 
 impl Player {
     pub fn revive(&self) -> Option<Player> {
-        unimplemented!("Revive this player")
+        if self.health > 0 {
+            None
+        } else {
+            Some(Player {
+                health: 100,
+                mana: if self.level >= 10 { Some(100) } else { None },
+                level: self.level,
+            })
+        }
     }
 
     pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
-        unimplemented!("Cast a spell of cost {}", mana_cost)
+        match self.mana {
+            Some(x) => {
+                if x >= mana_cost {
+                    self.mana = Some(x - mana_cost);
+                    2 * mana_cost
+                } else {
+                    0
+                }
+            }
+            None => {
+                self.health = self.health - min(self.health, mana_cost);
+                0
+            }
+        }
     }
 }
