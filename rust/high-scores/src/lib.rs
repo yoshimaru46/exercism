3d74980dc1c454a7cd49fc09db7c575244c9aef9
yoshimaru46 <yshr446@gmail.com>
2022-05-14 01:30:20 +0900

    wip

--- rust/high-scores/src/lib.rs
+++ rust/high-scores/src/lib.rs
@@ -1,27 +1,29 @@
 #[derive(Debug)]
-pub struct HighScores;
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
 
-impl HighScores {
-    pub fn new(scores: &[u32]) -> Self {
-        unimplemented!(
-            "Construct a HighScores struct, given the scores: {:?}",
-            scores
-        )
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        HighScores { scores }
     }
 
     pub fn scores(&self) -> &[u32] {
-        unimplemented!("Return all the scores as a slice")
+        self.scores
     }
 
     pub fn latest(&self) -> Option<u32> {
-        unimplemented!("Return the latest (last) score")
+        self.scores.last().cloned()
     }
 
     pub fn personal_best(&self) -> Option<u32> {
-        unimplemented!("Return the highest score")
+        self.scores.iter().max().cloned()
     }
 
     pub fn personal_top_three(&self) -> Vec<u32> {
-        unimplemented!("Return 3 highest scores")
+        let mut res_vec = self.scores.to_vec();
+        res_vec.sort_unstable_by(|a, b| b.cmp(a));
+        res_vec.truncate(3);
+        res_vec
     }
 }
