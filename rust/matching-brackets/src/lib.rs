0aa160a5bd08b6913fe346e7b9179cbaaca23cda
yoshimaru46 <yshr446@gmail.com>
2022-06-03 23:54:50 +0900

    WIP

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,6 +1,25 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+    let mut brackets: Vec<char> = Vec::new();
+    for c in string.chars() {
+        match c {
+            '(' | '{' | '[' => brackets.push(c),
+            ')' => {
+                if brackets.pop() != Some('(') {
+                    return false;
+                }
+            }
+            ']' => {
+                if brackets.pop() != Some('[') {
+                    return false;
+                }
+            }
+            '}' => {
+                if brackets.pop() != Some('{') {
+                    return false;
+                }
+            }
+            _ => (),
+        }
+    }
+    brackets.is_empty()
 }
