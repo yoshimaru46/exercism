7910fd8e7daa99b8544d66cc3a78d36b7a2656a2
yoshimaru46 <yshr446@gmail.com>
2022-06-05 20:59:40 +0900

    WIP

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,3 +1,12 @@
-export function age(planet: unknown, seconds: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function age(planet: string, seconds: number): number {
+  if (planet === "earth") {
+    let num = seconds;
+    num = num * 100;
+    num = num / 31557600;
+    num = Math.round(num);
+    num = num / 100;
+    return num;
+  }
+
+  return 0;
 }
