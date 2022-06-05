3deb364bc5e9f19f2ac28c9911c6d53e836230f1
yoshimaru46 <yshr446@gmail.com>
2022-06-05 19:20:01 +0900

    WIP

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,3 +1,18 @@
-export function toRna() {
-  throw new Error('Remove this statement and implement this function')
+const dnaMap: { [key: string]: string } = {
+  G: "C",
+  C: "G",
+  T: "A",
+  A: "U",
+};
+
+export function toRna(dna: string): string {
+  let rna = [];
+  for (const char of dna.split("")) {
+    const res = dnaMap[char];
+    if (!res) {
+      throw new Error("Invalid input DNA.");
+    }
+    rna.push(res);
+  }
+  return rna.join("");
 }
