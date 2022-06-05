3c9304c03cacf4b78846d54c67f6d31b0bbee643
yoshimaru46 <yshr446@gmail.com>
2022-06-05 22:29:46 +0900

    WIP

--- typescript/dnd-character/dnd-character.ts
+++ typescript/dnd-character/dnd-character.ts
@@ -1,9 +1,38 @@
 export class DnDCharacter {
-  public static generateAbilityScore(): number {
-    return Math.floor(Math.random() * 15) + 3;
+  public hitpoints: number;
+  public strength: number;
+  public dexterity: number;
+  public constitution: number;
+  public intelligence: number;
+  public wisdom: number;
+  public charisma: number;
+
+  constructor() {
+    this.strength = DnDCharacter.generateAbilityScore();
+    this.dexterity = DnDCharacter.generateAbilityScore();
+    this.constitution = DnDCharacter.generateAbilityScore();
+    this.intelligence = DnDCharacter.generateAbilityScore();
+    this.wisdom = DnDCharacter.generateAbilityScore();
+    this.charisma = DnDCharacter.generateAbilityScore();
+    this.hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution);
   }
 
+  public static generateAbilityScore(): number {
+    // create an array of 4 random number between 1 and 6
+    const diceResults: number[] = [];
+    for (let i = 0; i < 4; i++) {
+      diceResults.push(this.randomNum());
+    }
+    // sort the elements and remove the first
+    diceResults.sort().splice(0, 1);
+    // return the sum of the three greater elements in the arr
+    return diceResults.reduce((prev, cur) => prev + cur);
+  }
   public static getModifierFor(abilityValue: number): number {
-    throw new Error("Remove this statement and implement this function");
+    return Math.floor((abilityValue - 10) / 2);
+  }
+  // to create a random number an use it anywhere
+  public static randomNum(): number {
+    return Math.ceil(Math.random() * 6) + 1;
   }
 }
