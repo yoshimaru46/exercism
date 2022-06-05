223b423af2022c3ee641539dfdafcb34a49e9865
yoshimaru46 <yshr446@gmail.com>
2022-06-05 12:31:10 +0900

    WIP

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,3 +1,11 @@
-export function isLeap() {
-  throw new Error('Remove this statement and implement this function')
+export function isLeap(year: number): boolean {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 !== 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
 }
