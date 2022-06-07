2a204eba5244f168c97b17eb524f24e00e6734b0
yoshimaru46 <yshr446@gmail.com>
2022-06-07 10:26:57 +0900

    WIP

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -1,13 +1,21 @@
 export class Matrix {
-  constructor() {
-    throw new Error('Remove this statement and implement this function')
+  private _matrix: number[][];
+  constructor(matrixStr: string) {
+    this._matrix = matrixStr
+      .split("\n")
+      .map((row) => row.split(" ").map(Number));
   }
 
-  get rows(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  get rows(): number[][] {
+    return this._matrix.map((row) => row);
   }
 
-  get columns(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  get columns(): number[][] {
+    let cols = [];
+    for (let i = 0; i < this._matrix[0].length; i++) {
+      const column = this._matrix.map((row) => row[i]);
+      cols.push(column);
+    }
+    return cols;
   }
 }
