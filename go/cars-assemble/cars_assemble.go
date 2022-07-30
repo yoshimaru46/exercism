08993cba75362f78d3c4606a5675cd2c622ff6a3
yoshimaru46 <yshr446@gmail.com>
2022-07-30 21:12:22 +0900

    WIP

---
+++ go/cars-assemble/cars_assemble.go
@@ -0,0 +1,24 @@
+package cars
+
+const costOfOneCar = 10000
+const costOf10Cars = 95000
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour.
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return float64(productionRate) * successRate / 100
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate)) / 60
+}
+
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	bundleSize := carsCount / 10
+	mod := carsCount % 10
+
+	return uint((bundleSize * costOf10Cars) + (mod * costOfOneCar))
+}
