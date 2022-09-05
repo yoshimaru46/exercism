1ddbb5d68a86527eee82f8536de1e51250b6a7df
yoshimaru46 <yshr446@gmail.com>
2022-09-05 23:11:24 +0900

    WIP

--- typescript/secret-handshake/secret-handshake.test.ts
+++ typescript/secret-handshake/secret-handshake.test.ts
@@ -1,59 +1,59 @@
-import { commands } from './secret-handshake'
+import { commands } from "./secret-handshake";
 
-describe('Secret Handshake', () => {
-  describe('Create A Handshake For A Number', () => {
-    it('wink for 1', () => {
-      expect(commands(1)).toEqual(['wink'])
-    })
+describe("Secret Handshake", () => {
+  describe("Create A Handshake For A Number", () => {
+    it("wink for 1", () => {
+      expect(commands(1)).toEqual(["wink"]);
+    });
 
-    xit('double blink for 10', () => {
-      expect(commands(2)).toEqual(['double blink'])
-    })
+    it("double blink for 10", () => {
+      expect(commands(2)).toEqual(["double blink"]);
+    });
 
-    xit('close your eyes for 100', () => {
-      expect(commands(4)).toEqual(['close your eyes'])
-    })
+    it("close your eyes for 100", () => {
+      expect(commands(4)).toEqual(["close your eyes"]);
+    });
 
-    xit('jump for 1000', () => {
-      expect(commands(8)).toEqual(['jump'])
-    })
+    it("jump for 1000", () => {
+      expect(commands(8)).toEqual(["jump"]);
+    });
 
-    xit('combine two actions', () => {
-      expect(commands(3)).toEqual(['wink', 'double blink'])
-    })
+    it("combine two actions", () => {
+      expect(commands(3)).toEqual(["wink", "double blink"]);
+    });
 
-    xit('reverse two actions', () => {
-      expect(commands(19)).toEqual(['double blink', 'wink'])
-    })
+    it("reverse two actions", () => {
+      expect(commands(19)).toEqual(["double blink", "wink"]);
+    });
 
-    xit('reversing one action gives the same action', () => {
-      expect(commands(24)).toEqual(['jump'])
-    })
+    it("reversing one action gives the same action", () => {
+      expect(commands(24)).toEqual(["jump"]);
+    });
 
-    xit('reversing no actions still gives no actions', () => {
-      expect(commands(16)).toEqual([])
-    })
+    it("reversing no actions still gives no actions", () => {
+      expect(commands(16)).toEqual([]);
+    });
 
-    xit('all possible actions', () => {
+    it("all possible actions", () => {
       expect(commands(15)).toEqual([
-        'wink',
-        'double blink',
-        'close your eyes',
-        'jump',
-      ])
-    })
+        "wink",
+        "double blink",
+        "close your eyes",
+        "jump",
+      ]);
+    });
 
-    xit('reverse all possible actions', () => {
+    it("reverse all possible actions", () => {
       expect(commands(31)).toEqual([
-        'jump',
-        'close your eyes',
-        'double blink',
-        'wink',
-      ])
-    })
+        "jump",
+        "close your eyes",
+        "double blink",
+        "wink",
+      ]);
+    });
 
-    xit('do nothing for zero', () => {
-      expect(commands(0)).toEqual([])
-    })
-  })
-})
+    it("do nothing for zero", () => {
+      expect(commands(0)).toEqual([]);
+    });
+  });
+});
--- typescript/secret-handshake/secret-handshake.ts
+++ typescript/secret-handshake/secret-handshake.ts
@@ -1,3 +1,9 @@
-export function commands() {
-  throw new Error('Remove this statement and implement this function')
+export function commands(num: number): string[] {
+  const commands = [];
+  if (num & 1) commands.push("wink");
+  if (num & 2) commands.push("double blink");
+  if (num & 4) commands.push("close your eyes");
+  if (num & 8) commands.push("jump");
+  if (num & 16) commands.reverse();
+  return commands;
 }
