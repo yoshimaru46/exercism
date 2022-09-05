18ce39ceb1c8179d1db0a7051145dd1464991bc3
yoshimaru46 <yshr446@gmail.com>
2022-09-05 23:16:56 +0900

    WIP

--- typescript/binary-search/binary-search.ts
+++ typescript/binary-search/binary-search.ts
@@ -1,3 +1,15 @@
-export function find(haystack: unknown, needle: unknown): number | never {
-  throw new Error('Remove this statement and implement this function')
+export function find(haystack: number[], needle: number): number | unknown {
+  let left = 0;
+  let right = haystack.length - 1;
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2);
+    if (haystack[middle] === needle) {
+      return middle;
+    } else if (haystack[middle] < needle) {
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
+  throw new Error("Value not in array");
 }
