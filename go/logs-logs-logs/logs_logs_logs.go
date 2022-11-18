42d57a51cf3e1355deeb70810fa6194a2a5a717e
Yoshihiro Saito <yshr446@gmail.com>
2022-11-18 23:15:42 +0900

    WIP

---
+++ go/logs-logs-logs/logs_logs_logs.go
@@ -0,0 +1,37 @@
+package logs
+
+var charMap = map[rune]string{
+	'❗': "recommendation",
+	'🔍': "search",
+	'☀': "weather",
+}
+
+// Application identifies the application emitting the given log.
+func Application(log string) string {
+	for _, rune := range log {
+		if app, exists := charMap[rune]; exists {
+			return app
+		}
+	}
+	return "default"
+}
+
+// Replace replaces all occurrences of old with new, returning the modified log
+// to the caller.
+func Replace(log string, oldRune, newRune rune) string {
+	runes := []rune(log)
+
+	for i, r := range runes {
+		if r == oldRune {
+			runes[i] = newRune
+		}
+	}
+
+	return string(runes)
+}
+
+// WithinLimit determines whether or not the number of characters in log is
+// within the limit.
+func WithinLimit(log string, limit int) bool {
+	return len([]rune(log)) <= limit
+}
